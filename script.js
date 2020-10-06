107e25af3d23c4d306ab3c490b1657300bd0fb34
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-10-05 17:07:55 -0700

    Created template for note calculate function

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // A-sync function and a math filter required. 
 // After getting input from type of music, it should prompt 
 // filter to show elements within that genre.
+
+function calculate(){
+    
+
+    var jam = document.getElementById("jam").nodeValue;
+    var strings = document.getElementById("strings").nodeValue;
+    var frets = document.getElementById("frets").nodeValue;
+}
