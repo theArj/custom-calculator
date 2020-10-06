aa33b748e621ce57bdedc2a2ebb47c662a420fdc
Arjun Ramesh <69367261+theArj@users.noreply.github.com>
2020-10-05 19:16:08 -0700

    Final Code Revised: updated README.md and cleaned up unnecessary commented-out code

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// console.log("Hello World");
-
+// From CoWork mentor:
 // A-sync function and a math filter required.
 // After getting input from type of music, it should prompt
-// filter to show elements within that genre.
+// filter to show strings and frets within that genre.
 
 // Create a function that takes the number of strings and multiplies
 //  it by the number of frets to give the total number of notes playable
@@ -17,27 +16,24 @@ const inputStrings = document.querySelector("#strings");
 const inputFrets = document.querySelector("#frets");
 const displayTotal = document.querySelector("#total_notes");
 
-// inputJam.addEventListener("input", calculate);
-// inputStrings.addEventListener("input", calculate);
-// inputFrets.addEventListener("input", calculate);
 calculatebutton.addEventListener("click", calculate);
 
 function calculate() {
-  console.log("hello world");
   //   Because A-sync and math fileters are required to omit certain
   //      number of strings and frets according to genre, the "jam" variable
   //      will be disabled for the time being.
+
   //   const jam = document.getElementById("jam").nodeValue;
   const strings = parseInt(inputStrings.value);
   const frets = parseInt(inputFrets.value);
   const total = strings * frets;
   console.log(total);
   displayTotal.innerHTML = total;
-  //   total = total.toFixed(1);
-  //   document.getElementById("total_notes").style.display = "block";
-  //   document.getElementById("notes").innerHTML = total;
 }
 
 document.getElementById("btnClick").onclick = function () {
   calculate();
 };
+
+// Collaborative with Jackie Chan to fix getQuerySelector (previously getElementById
+//  and was not working).
